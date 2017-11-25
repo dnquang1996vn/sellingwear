79e9277b81cf4adb206044a1de41be73814e1975
Dung Dinh <dinhanhdung1996@gmail.com>
2017-11-25 09:34:06 +0700

    adding use case diagram

---
+++ app/User/Admin.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: dungda_pc
+ * Date: 24/11/2017
+ * Time: 11:04
+ */
+
+namespace App\User;
+
+
+use App\User;
+
+class Admin extends User
+{
+    public function deleteUser(){
+
+    }
+
+    public function updateUser(){
+
+    }
+
+    public function createUser(){
+
+    }
+
+    public function createProduct(){
+
+    }
+
+    public function updateProduct(){
+
+    }
+
+    public function deleteProduct(){
+
+    }
+
+}
