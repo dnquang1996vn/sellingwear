6a0e5087029806d5bf466227b0a653cd0d2632bd
quangkhovigaitheo <nhatquangprovodoi@gmail.com>
2017-11-26 15:35:12 +0700

    d

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,29 +10,29 @@ use App\Model\Product;
 class CategoryController extends Controller
 {
     public function create(CategoryRequest $request)
-    {	
-    	$category = new Category;
-    	$category->name = $request->name;
-    	$category->save();
-    	return redirect()->route('show_category');
+    {   
+        $category = new Category;
+        $category->name = $request->name;
+        $category->save();
+        return redirect()->route('show_category');
     }
 
     public function show()
     {
-    	$categories = Category::all();
-    	return view('category_show')->with('categories', $categories);
+        $categories = Category::all();
+        return view('category.category_show')->with('categories', $categories);
     }
 
     public function delete($id)
     {
-    	$category = Category::destroy($id);
-    	return redirect()->route('show_category');
+        $category = Category::destroy($id);
+        return redirect()->route('show_category');
     }
 
     public function view($id)
     {
         $categories = Category::all();
         $products = Product::orderBy('id','asc')->where('category_id', $id)->get();
-        return view('product_list')->with('products', $products)->with('categories', $categories);
+        return view('product.product_list')->with('products', $products)->with('categories', $categories);
     }
 }
