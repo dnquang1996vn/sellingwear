7cd0c5e23ea438d51ad2abca8a5b959101113111
quangkhovigaitheo <nhatquangprovodoi@gmail.com>
2017-11-30 20:22:52 +0700

    migrate

--- database/migrations/2017_11_30_131824_cart.php
+++ database/migrations/2017_11_30_131824_cart.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImageTable extends Migration
+class Cart extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateImageTable extends Migration
      */
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id');
-            $table->string('url');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateImageTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('image');
+        Schema::dropIfExists('carts');
     }
 }
