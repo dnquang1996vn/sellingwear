bc94b999e14f9af3294e99404c70c61ad8891bc7
quangkhovigaitheo <nhatquangprovodoi@gmail.com>
2017-11-30 21:18:12 +0700

    add migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
         Blade::if('isAdmin', function () {
             return auth()->check() && auth()->user()->isAdmin();
         });
-        $categories = Category::all();
-        View::share('categories', $categories);
+        // $categories = Category::all();
+        // View::share('categories', $categories);
     }
 
     /**
